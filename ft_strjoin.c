fa8c169b1c18c7136f4f01b89219928d5b224998
Glak Gevorgian <sp1tak.gg@gmail.com>
2025-01-15 13:26:34 +0400

    Изменил функцию ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,38 +6,36 @@
 /*   By: ggevorgi <sp1tak.gg@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 21:45:01 by ggevorgi          #+#    #+#             */
-/*   Updated: 2024/10/27 22:53:08 by ggevorgi         ###   ########.fr       */
+/*   Updated: 2025/01/15 13:26:02 by ggevorgi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_strjoin_helper(char *s1, const char *s2, size_t index)
-{
-	size_t	i;
-
-	i = 0;
-	while (s2[i])
-		s1[index++] = s2[i++];
-}
-
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	len;
 	char	*res;
+	int		i;
+	int		j;
 
-	if (!s1 && !s2)
-		return (ft_strdup(""));
-	if (!s1)
-		return (ft_strdup(s2));
-	if (!s2)
-		return (ft_strdup(s1));
-	len = ft_strlen(s1) + ft_strlen(s2);
-	res = (char *)malloc(sizeof(char) * (len + 1));
+	if (!s1 || !s2)
+		return (NULL);
+	i = 0;
+	j = 0;
+	res = (char *)malloc(sizeof(char *) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!res)
 		return (NULL);
-	ft_strjoin_helper(res, s1, 0);
-	ft_strjoin_helper(res, s2, ft_strlen(s1));
-	res[len] = '\0';
+	while (s1[i] != '\0')
+	{
+		res[i] = s1[i];
+		++i;
+	}
+	while (s2[j] != '\0')
+	{
+		res[i] = s2[j];
+		++i;
+		++j;
+	}
+	res[i] = '\0';
 	return (res);
 }
